296efd244df3252a83797335be1c8be46a0abfb9
massndp <bayu8989.88@gmail.com>
2021-12-05 11:51:42 +0700

    Dashboard api

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -3,12 +3,63 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        //
+        //count invoice by status
+        $pending = Invoice::where('status', 'pending')->count();
+        $success = Invoice::where('status', 'success')->count();
+        $expired = Invoice::where('status', 'expired')->count();
+        $failed = Invoice::where('status', 'failed')->count();
+
+        //year
+        $year = date('Y');
+
+        //chart
+        $transactions = DB::table('invoices')
+            ->addSelect(DB::raw('SUM(grand_total as grand_total'))
+            ->addSelect(DB::raw('MONTH(created_at) as month'))
+            ->addSelect(DB::raw('MONTHNAME(created_at) as month_name'))
+            ->addSelect(DB::raw('YEAR(created_at) as year'))
+            ->whereYear('created_at', '=', $year)
+            ->where('status', 'success')
+            ->groupBy('month')
+            ->orderByRaw('month ASC')
+            ->get();
+
+
+        if (count($transactions)) {
+            foreach ($transactions as $result) {
+                $month_name[] = $result->month_name;
+                $grand_total[] = $result->grand_total;
+            }
+        } else {
+            $month_name = "";
+            $grand_total = "";
+        }
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Statistik',
+            'data' => [
+                'count' => [
+                    'pending' => $pending,
+                    'success' => $success,
+                    'expired' => $expired,
+                    'failed' => $failed,
+                ],
+
+                'chart' => [
+                    'month_name' => $month_name,
+                    'grand_total' => $grand_total
+                ]
+            ]
+        ], 200);
     }
 }
